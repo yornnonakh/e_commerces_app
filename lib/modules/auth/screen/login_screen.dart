ea4c742929223d2796dab915b86fec824d6dae10
Nona Yorn <yornnonakh@gmail.com>
2026-03-16 08:18:59 +0700

    add the new featue 16, 03, 2026

--- lib/modules/auth/screen/login_screen.dart
+++ lib/modules/auth/screen/login_screen.dart
@@ -1,12 +1,8 @@
-import 'package:e_commerces/app/theme/app_colors.dart';
 import 'package:e_commerces/app/theme/app_text_style.dart';
-import 'package:e_commerces/modules/home/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
 
 class Login extends StatefulWidget {
-  Login({super.key});
+  const Login({super.key});
 
   @override
   State<Login> createState() => _LoginState();
