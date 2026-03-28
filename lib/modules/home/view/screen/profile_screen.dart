86937a1c03217dfc683201d88397bb2423460799
Nona Yorn <yornnonakh@gmail.com>
2026-03-28 10:59:46 +0700

    add the new feature

--- lib/modules/home/view/screen/profile_screen.dart
+++ lib/modules/home/view/screen/profile_screen.dart
@@ -1,5 +1,10 @@
 import 'package:e_commerces/app/config/app_assets.dart';
+import 'package:e_commerces/app/theme/app_colors.dart';
+import 'package:e_commerces/app/theme/app_text_style.dart';
+import 'package:e_commerces/modules/home/view/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -15,6 +20,19 @@ class ProfileScreen extends StatelessWidget {
             image: AssetImage(AppAssets.backgroundcover),
             fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Text("User Profile", style:  AppTextStyle.categoryTextStyle,),
+                IconButton(onPressed: (){
+                  Get.to(HomeScreen());
+                }, icon: Icon(Icons.person, color: AppColors.backgroundLight,))
+              ],
+            )
+          ],
         ),
       ),
     );
